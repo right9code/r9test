ecff6de6f13bbdbc85f5c0a9f8672f496cb2c6a8
right9code <right9code@gmail.com>
2025-07-22 10:52:53 +0530

    Update partition_disk.sh

--- partition_disk.sh
+++ partition_disk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DISK="/dev/vda"
 # For non-dual-boot (fresh install), uncomment the line below to wipe the disk and create a new GPT:
-# parted -s "$DISK" mklabel gpt
+parted -s "$DISK" mklabel gpt
 parted -s "$DISK" mkpart primary fat32 0% 512MiB
 NEW_EFI_PART_NUM=$(parted -s "$DISK" print | grep -E '^\s*[0-9]+' | tail -n 1 | awk '{print $1}')
 parted -s "$DISK" set "$NEW_EFI_PART_NUM" esp on
